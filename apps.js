58d13568fe6c9f397b4fea7549efcfecb66703d0
MD FAIZUDDIN <84907848+Faiz528@users.noreply.github.com>
2023-05-11 12:00:05 +0530

    Update apps.js(retreiving data feature added)

--- apps.js
+++ apps.js
@@ -1,3 +1,11 @@
 const express = require('express')
 
 const db = require('./database.js')
+
+db.execute('SELECT * FROM products')
+.then((result)=>{
+    console.log(result[0])
+})
+.catch((err)=>{
+    console.log(err)
+})
